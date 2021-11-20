7c90f69d708e27958a4806564c818017e42ee2af
Đức Anh Tài <datai28599@gmail.com>
2021-11-20 18:07:10 +0700

    backup template data

---
+++ workspace/sql/sc_template_council_roles_1637403278396.sql
@@ -0,0 +1,3 @@
+insert into sc_template_council_roles(template_id,council_role_id) values('6','1');
+insert into sc_template_council_roles(template_id,council_role_id) values('6','2');
+insert into sc_template_council_roles(template_id,council_role_id) values('6','3');
---
+++ workspace/sql/tp_topic_students_1637318693068.sql
@@ -0,0 +1,2 @@
+insert into tp_topic_students(topic_id,student_id) values('14','42');
+insert into tp_topic_students(topic_id,student_id) values('10','41');
