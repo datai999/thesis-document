1aa5516ddf485238af5d85f56e682969395be845
Đức Anh Tài <datai28599@gmail.com>
2021-11-29 17:19:26 +0700

    warning conflict timeline

---
+++ workspace/sql/tp_student_1638172460359.sql
@@ -0,0 +1,7 @@
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('1','2021-11-29 00:31:18.491107','false','2021-11-29 00:31:18.491107','false','31','1');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('2','2021-11-29 00:31:18.491107','false','2021-11-29 00:31:18.491107','true','32','1');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('3','2021-11-29 00:31:18.491107','false','2021-11-29 00:31:18.491107','true','33','5');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('4','2021-11-29 00:31:18.491107','false','2021-11-29 00:31:18.491107','true','34','5');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('5','2021-11-29 00:31:18.492722','false','2021-11-29 00:31:18.492722','true','32','21');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('6','2021-11-29 00:31:18.492722','false','2021-11-29 00:31:18.492722','true','33','25');
+insert into tp_student(id,created_at,deleted,updated_at,mid_pass,student_id,topic_id) values('7','2021-11-29 00:31:18.492722','false','2021-11-29 00:31:18.492722','true','34','25');
---
+++ workspace/sql/tp_topic_review_teachers_1638172048217.sql
@@ -0,0 +1,7 @@
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('1','12');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('5','6');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('10','11');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('21','12');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('25','6');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('25','11');
+insert into tp_topic_review_teachers(topic_id,review_teacher_id) values('30','11');
