ff3fa1b1631950e006d30b979f5e0947af6124bc
Đức Anh Tài <datai28599@gmail.com>
2021-12-04 01:36:53 +0700

    student notification

---
+++ workspace/sql/us_notification_1638556322781.sql
@@ -0,0 +1,1 @@
+insert into us_notification(id,created_at,deleted,updated_at,message,seen,receiver_id) values('113','2021-12-04 01:30:59.382664','false','2021-12-04 01:30:59.382664','<user id=36>Nguyễn y</user> đã hủy đăng ký đề tài [Xây dựng hệ thống QUESTION ANSWERING hỗ trợ các bạn học sinh học môn Lịch Sử - null]','false','56');
